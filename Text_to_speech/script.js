afaed783a37d34ddcb97a946598a8ebd5f1b75d7
Yash poniya <96628229+yashponiya001@users.noreply.github.com>
2024-02-05 21:58:27 +0530

    Add files via upload

---
+++ Text_to_speech/script.js
@@ -0,0 +1,22 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => {
+        const option = new Option(voice.name, i);
+        voiceSelect.options.add(option);
+    });
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
